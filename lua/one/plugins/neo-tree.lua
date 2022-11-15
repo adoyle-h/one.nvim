2adfdcf09c676e733aa826f1d06d6967be21c165
ADoyle <adoyle.h@gmail.com>
2022-11-15 16:10:06 +0800

    feat(neo-tree): add keymap "Y" to copy filepath/filename of the node under cursor

--- lua/one/plugins/neo-tree.lua
+++ lua/one/plugins/neo-tree.lua
@@ -14,6 +14,41 @@ local hide_by_name = {}
 util.tbl_concat(hide_by_name, config.ignore.fileSearch.directories)
 util.tbl_concat(hide_by_name, config.ignore.fileSearch.files)
 
+local function keymapCopy(state)
+	-- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+	-- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+	local node = state.tree:get_node()
+	local filepath = node:get_id()
+	local filename = node.name
+	local modify = vim.fn.fnamemodify
+
+	local results = {
+		filepath,
+		modify(filepath, ':.'),
+		modify(filepath, ':~'),
+		filename,
+		modify(filename, ':r'),
+		modify(filename, ':e'),
+	}
+
+	-- absolute path to clipboard
+	local i = vim.fn.inputlist({
+		'Choose to copy to clipboard:',
+		'1. Absolute path: ' .. results[1],
+		'2. Path relative to CWD: ' .. results[2],
+		'3. Path relative to HOME: ' .. results[3],
+		'4. Filename: ' .. results[4],
+		'5. Filename without extension: ' .. results[5],
+		'6. Extension of the filename: ' .. results[6],
+	})
+
+	if i > 0 then
+		local result = results[i]
+		if not result then return print('Invalid choice: ' .. i) end
+		vim.fn.setreg('"', result)
+	end
+end
+
 M.defaultConfig = function()
 	local conf = {
 		'neotree',
@@ -112,6 +147,7 @@ M.defaultConfig = function()
 					['z'] = 'close_all_nodes',
 					['Z'] = 'expand_all_nodes',
 					['d'] = 'delete',
+					['Y'] = keymapCopy,
 					['y'] = 'copy_to_clipboard',
 					['x'] = 'cut_to_clipboard',
 					['p'] = 'paste_from_clipboard',
