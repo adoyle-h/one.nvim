6077cfb22cc603ec33181f75da6e7e65f500cbee
ADoyle <adoyle.h@gmail.com>
2023-02-09 00:30:43 +0800

    fix: remove keymaps of table-mode && add commands TableModeToggleGFM and TableModeToggleNormal

--- lua/one/plugins/table.lua
+++ lua/one/plugins/table.lua
@@ -5,27 +5,6 @@ local M = {
 	on = 'TableModeToggle',
 }
 
-local function changeTableMode()
-	local b = vim.b
-	if (b.table_mode_mode == 'Normal') then
-		b.table_mode_corner = '|'
-		b.table_mode_separator = '|'
-		b.table_mode_fillchar = '-'
-		b.table_mode_corner_corner = '|'
-		b.table_mode_align_char = ':'
-		b.table_mode_mode = 'GFM'
-	else
-		b.table_mode_corner = '+'
-		b.table_mode_separator = '|'
-		b.table_mode_fillchar = '-'
-		b.table_mode_corner_corner = '+'
-		b.table_mode_align_char = ':'
-		b.table_mode_mode = 'Normal'
-	end
-
-	print('Table Mode: ' .. b.table_mode_mode)
-end
-
 M.defaultConfig = {
 	{ 'table' },
 	{
@@ -38,27 +17,32 @@ M.defaultConfig = {
 }
 
 function M.config(config)
-	local conf = config.table
 	local g = vim.g
-	g.table_mode_motion_up_map = conf.motion_up_map
-	g.table_mode_motion_down_map = conf.motion_down_map
-	g.table_mode_motion_left_map = conf.motion_left_map
-	g.table_mode_motion_right_map = conf.motion_right_map
-	g.table_mode_map_prefix = conf.map_prefix
+	for key, value in pairs(config.table) do g['table_mode_' .. key] = value end
 end
 
-M.keymaps = {
-	{
-		'n',
-		'<leader>Tm',
-		function()
-			vim.b.table_mode_mode = 'GFM'
-			vim.cmd.TableModeToggle()
-		end,
-		{ desc = 'Toggle TableMode' },
-	},
+M.commands = {
+	TableModeToggleGFM = function()
+		local b = vim.b
+		b.table_mode_corner = '|'
+		b.table_mode_separator = '|'
+		b.table_mode_fillchar = '-'
+		b.table_mode_corner_corner = '|'
+		b.table_mode_align_char = ':'
 
-	{ 'n', '<leader>Tc', changeTableMode, { desc = 'Change TableMode' } },
+		vim.cmd.TableModeToggle()
+	end,
+
+	TableModeToggleNormal = function()
+		local b = vim.b
+		b.table_mode_corner = '+'
+		b.table_mode_separator = '|'
+		b.table_mode_fillchar = '-'
+		b.table_mode_corner_corner = '+'
+		b.table_mode_align_char = ':'
+
+		vim.cmd.TableModeToggle()
+	end,
 }
 
 return M
