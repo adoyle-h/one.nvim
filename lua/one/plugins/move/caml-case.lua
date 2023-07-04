f4e5b1333b2054b3e9b624cab6c3cf2355a4a5a5
ADoyle <adoyle.h@gmail.com>
2023-07-04 23:31:38 +0800

    fix: remove cursor motion keymaps W, B, E
    Recover back to native keymaps

--- lua/one/plugins/move/caml-case.lua
+++ lua/one/plugins/move/caml-case.lua
@@ -4,8 +4,5 @@ return {
 		{ '', 'w', '<Plug>CamelCaseMotion_w', { silent = true } },
 		{ '', 'b', '<Plug>CamelCaseMotion_b', { silent = true } },
 		{ '', 'e', '<Plug>CamelCaseMotion_e', { silent = true } },
-		{ '', 'W', 'w', { noremap = true } },
-		{ '', 'B', 'b', { noremap = true } },
-		{ '', 'E', 'e', { noremap = true } },
 	},
 }
