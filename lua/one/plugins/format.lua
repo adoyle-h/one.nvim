675b0f867890e48562ca8624e566053801be5662
ADoyle <adoyle.h@gmail.com>
2023-09-24 21:34:35 +0800

    add formater.nvim

--- lua/one/plugins/format.lua
+++ lua/one/plugins/format.lua
@@ -14,5 +14,36 @@ return {
 		},
 
 		{ 'tpope/vim-sleuth', desc = 'Auto set shiftwidth expandtab based on text context' },
+
+		{
+			'mhartington/formatter.nvim',
+
+			config = function(config)
+				require('formatter').setup(config.formatter)
+			end,
+
+			defaultConfig = {
+				'formatter',
+				{
+					-- Enable or disable logging
+					logging = true,
+					-- Set the log level
+					log_level = vim.log.levels.WARN,
+					-- All formatter configurations are opt-in
+					filetype = {
+						-- Formatter configurations for filetype "lua" go here
+						-- and will be executed in order
+
+						-- Use the special "*" filetype for defining formatter configurations on
+						-- any filetype
+						-- ['*'] = {
+						-- 	-- "formatter.filetypes.any" defines default configurations for any
+						-- 	-- filetype
+						-- 	require('formatter.filetypes.any').remove_trailing_whitespace,
+						-- },
+					},
+				},
+			},
+		},
 	},
 }
