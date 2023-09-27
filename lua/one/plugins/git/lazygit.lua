8755c30ff5ada915e39cdd46b87bacaa2416b457
ADoyle <adoyle.h@gmail.com>
2023-09-27 21:07:07 +0800

    fix(lazygit): option 'floating_window_corner_chars' is deprecated

--- lua/one/plugins/git/lazygit.lua
+++ lua/one/plugins/git/lazygit.lua
@@ -13,7 +13,7 @@ return {
 		{
 			floating_window_winblend = 0, -- transparency of floating window
 			floating_window_scaling_factor = 0.8, -- scaling factor for floating window
-			floating_window_corner_chars = { '╭', '╮', '╰', '╯' }, -- customize lazygit popup window corner characters
+			floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' }, -- customize lazygit popup window border characters
 			floating_window_use_plenary = 1, -- use plenary.nvim to manage floating window if available
 			use_neovim_remote = 1, -- fallback to 0 if neovim-remote is not installed
 			use_custom_config_file_path = 0, -- config file path is evaluated if this value is 1
