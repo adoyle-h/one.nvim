519245883acfc6efb00673f8d91d0807871d789d
ADoyle <adoyle.h@gmail.com>
2023-10-03 05:15:30 +0800

    feat!: upgrade indent-blankline to v3
    Breaking Change:
    
    - config.indentLine => config['indent-blankline']
    - All config options have changed. Please see ":h ibl.config".

--- lua/one/plugins/indent-line.lua
+++ lua/one/plugins/indent-line.lua
@@ -6,35 +6,60 @@ local M = {
 	desc = 'Show indent line',
 	after = 'nvim-treesitter',
 
-	highlights = {
-		IndentBlanklineChar = { fg = colors.grey1 },
-		IndentBlanklineContextChar = { fg = colors.purple },
+	highlights = { --
+		IblIndent = { fg = colors.grey1 },
+		IblWhitespace = { fg = colors.grey1 },
+		IblScope = { fg = colors.purple },
 	},
 }
 
 M.defaultConfig = {
-	'indentLine',
-	{
-		line = '▏', -- '┊', '', '⎜', '⎸', '│', '⎜', '⎜', '⎜'
-		excludeFileType = {
-			'alpha', -- goolord/alpha-nvim, see plugins/alpha.lua
+	'indent-blankline',
+	{ -- :h ibl.config
+		enabled = true,
+		debounce = 200,
+		viewport_buffer = { min = 30, max = 500 },
+		indent = {
+			char = '▏', -- '▎', '┊', '', '⎜', '⎸', '│', '⎜', '⎜', '⎜'
+			tab_char = nil,
+			highlight = 'IblIndent',
+			smart_indent_cap = true,
+			priority = 1,
 		},
-		opts = { -- :h indent_blankline
-			-- space_char_blankline = " ",
-			show_current_context = true,
-			show_current_context_start = false,
+		whitespace = { highlight = 'IblWhitespace', remove_blankline_trail = false },
+		scope = {
+			enabled = true,
+			char = '▏',
+			show_start = false, -- Shows an underline on the first line of the scope
+			show_end = false, -- Shows an underline on the last line of the scope
+			injected_languages = true,
+			highlight = 'IblScope',
+			priority = 1024,
+			include = { node_type = {} },
+			exclude = {
+				language = {},
+				node_type = { ['*'] = { 'source_file', 'program' }, lua = { 'chunk' }, python = { 'module' } },
+			},
+		},
+		exclude = {
+			filetypes = {
+				'lspinfo',
+				'packer',
+				'checkhealth',
+				'help',
+				'man',
+				'gitcommit',
+				'TelescopePrompt',
+				'TelescopeResults',
+				'alpha',
+			},
+			buftypes = { 'terminal', 'nofile', 'quickfix', 'prompt' },
 		},
 	},
 }
 
-function M.config()
-	local conf = config.indentLine
-
-	vim.g.indent_blankline_char = conf.line
-	vim.g.indent_blankline_context_char = conf.line
-	vim.g.indent_blankline_filetype_exclude = conf.excludeFileType
-
-	require('indent_blankline').setup(conf.opts)
+function M.config(config)
+	require('ibl').setup(config['indent-blankline'])
 end
 
 return M
