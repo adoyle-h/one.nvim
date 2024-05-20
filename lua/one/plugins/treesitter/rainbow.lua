8e459f99bec37c8b902985f6fec1a711aafba26a
ADoyle <adoyle.h@gmail.com>
2024-05-20 15:32:53 +0800

    fix(rainbow): nvim-ts-rainbow is achieved, use rainbow-delimiters.nvim instead of
    User should install rainbow-delimiters.nvim via plugin manager

--- lua/one/plugins/treesitter/rainbow.lua
+++ lua/one/plugins/treesitter/rainbow.lua
@@ -1,28 +1,60 @@
 return {
-	'p00f/nvim-ts-rainbow',
+	'hiphish/rainbow-delimiters.nvim',
 	desc = 'Rainbow brackets',
 	after = 'nvim-treesitter',
 
 	config = function(config)
-		require('nvim-treesitter.configs').setup { rainbow = config.rainbow }
+		local rainbow_delimiters = require 'rainbow-delimiters'
+		local tablex = require('pl.tablex')
+
+		local conf = config.rainbow
+
+		conf.strategy = tablex.map(function(name)
+			return rainbow_delimiters.strategy[name]
+		end, conf.strategy)
+
+		require 'rainbow-delimiters.setup'.setup(conf)
 	end,
 
 	defaultConfig = {
 		'rainbow',
 		{
-			enable = true,
-			-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-			extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-			max_file_lines = nil, -- Do not enable for files with more than n lines, int
-			colors = { -- table of hex strings
-				-- LuaFormatter off
-				'#589CFF', '#1F4FFF', '#823EF8', '#00a887', '#b8afbf', '#82ab00', '#b03233',
-				'#12cf5f', '#8D2E8D', '#8787ff', '#82875f',
-				-- LuaFormatter on
+			-- NOTE: the value should be string
+			strategy = {
+				[''] = 'global',
+				-- commonlisp = 'local',
 			},
-
-			termcolors = { -- table of colour name strings
+			query = {
+				[''] = 'rainbow-delimiters',
+				latex = 'rainbow-blocks',
+			},
+			highlight = {
+				'RainbowDelimiter_1',
+				'RainbowDelimiter_2',
+				'RainbowDelimiter_3',
+				'RainbowDelimiter_4',
+				'RainbowDelimiter_5',
+				'RainbowDelimiter_6',
+				'RainbowDelimiter_7',
+				'RainbowDelimiter_8',
+				'RainbowDelimiter_9',
+				'RainbowDelimiter_10',
+			},
+			blacklist = {
 			},
 		},
+	},
+
+	highlights = {
+		RainbowDelimiter_1 = { fg = '#b54b33' },
+		RainbowDelimiter_2 = { fg = '#1F4FFF' },
+		RainbowDelimiter_3 = { fg = '#823EF8' },
+		RainbowDelimiter_4 = { fg = '#00a887' },
+		RainbowDelimiter_5 = { fg = '#82605f' },
+		RainbowDelimiter_6 = { fg = '#82ab00' },
+		RainbowDelimiter_7 = { fg = '#3e41be' },
+		RainbowDelimiter_8 = { fg = '#12cf5f' },
+		RainbowDelimiter_9 = { fg = '#8D2E8D' },
+		RainbowDelimiter_10 = { fg = '#8787ff' },
 	},
 }
