8d0b02213850a573226edf234aa360d731ed4bc0
ADoyle <adoyle.h@gmail.com>
2024-06-24 03:47:02 +0800

    fix(gitsigns): some config options are deprecated
    removed support for yadm, see https://github.com/lewis6991/gitsigns.nvim/pull/1055
    config.signs.<kind>.[hl,numhl,linehl] are now deprecated, see https://github.com/lewis6991/gitsigns.nvim/pull/1054

--- lua/one/plugins/git/sign.lua
+++ lua/one/plugins/git/sign.lua
@@ -33,48 +33,19 @@ M.defaultConfig = {
 	'gitsigns',
 	{
 		signs = {
-			untracked = { --
-				hl = 'GitSignsAdd',
-				text = '┃',
-				numhl = 'GitSignsAddNr',
-				linehl = 'GitSignsAddLn',
-			},
-			add = { --
-				hl = 'GitSignsAdd',
-				text = '┃',
-				numhl = 'GitSignsAddNr',
-				linehl = 'GitSignsAddLn',
-			},
-			change = {
-				hl = 'GitSignsChange',
-				text = '┃',
-				numhl = 'GitSignsChangeNr',
-				linehl = 'GitSignsChangeLn',
-			},
-			delete = {
-				hl = 'GitSignsDelete',
-				text = '_',
-				numhl = 'GitSignsDeleteNr',
-				linehl = 'GitSignsDeleteLn',
-			},
-			topdelete = {
-				hl = 'GitSignsDelete',
-				text = '‾',
-				numhl = 'GitSignsDeleteNr',
-				linehl = 'GitSignsDeleteLn',
-			},
-			changedelete = {
-				hl = 'GitSignsChange',
-				text = '┃',
-				numhl = 'GitSignsChangeNr',
-				linehl = 'GitSignsChangeLn',
-			},
+			add = { text = '┃' },
+			change = { text = '┃' },
+			delete = { text = '_' },
+			topdelete = { text = '‾' },
+			changedelete = { text = '┃' },
+			untracked = { text = '┃' },
 		},
 		signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 		numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
 		linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 		word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
 		watch_gitdir = { interval = 1000, follow_files = true },
+		auto_attach = true,
 		attach_to_untracked = true,
 		current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 		current_line_blame_opts = {
@@ -82,6 +53,7 @@ M.defaultConfig = {
 			virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
 			delay = 100,
 			ignore_whitespace = false,
+			virt_text_priority = 100,
 		},
 		current_line_blame_formatter = '[<author_time:%Y/%m/%d %H:%M:%S> <author>] <summary>',
 		sign_priority = 6,
@@ -96,7 +68,6 @@ M.defaultConfig = {
 			row = 0,
 			col = 1,
 		},
-		yadm = { enable = false },
 		-- on_attach = function(bufnr) end,
 	},
 
