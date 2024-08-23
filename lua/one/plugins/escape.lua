d225d91e7358276878b1cfba1803a5a1becc515b
ADoyle <adoyle.h@gmail.com>
2024-08-23 14:49:31 +0800

    fix(escape): breaking change in max397574/better-escape.nvim
    see https://github.com/max397574/better-escape.nvim#rewrite

--- lua/one/plugins/escape.lua
+++ lua/one/plugins/escape.lua
@@ -8,14 +8,38 @@ return {
 	defaultConfig = {
 		'escape',
 		{
-			mapping = { 'jk' }, -- a table with mappings to use
 			timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-			clear_empty_lines = false, -- clear line after escaping if there is only whitespace
-			keys = '<Esc>', -- keys used for escaping, if it is a function will use the result everytime
-			-- example(recommended)
-			-- keys = function()
-			--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-			-- end,
+
+			default_mappings = false,
+
+			mappings = {
+				i = {
+					j = {
+						-- These can all also be functions
+						k = '<Esc>',
+					},
+				},
+				c = {
+					j = {
+						k = '<Esc>',
+					},
+				},
+				t = {
+					j = {
+						k = '<Esc>',
+					},
+				},
+				v = {
+					j = {
+						k = '<Esc>',
+					},
+				},
+				s = {
+					j = {
+						k = '<Esc>',
+					},
+				},
+			},
 		},
 	},
 
