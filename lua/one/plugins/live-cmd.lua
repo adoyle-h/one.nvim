d03b783ddaa6ec0278c3a2fb5c5b2d83bb8dead8
ADoyle <adoyle.h@gmail.com>
2024-10-03 03:49:50 +0800

    fix: change config for latest version of live-cmd.nvim

--- lua/one/plugins/live-cmd.lua
+++ lua/one/plugins/live-cmd.lua
@@ -8,26 +8,18 @@ return {
 	defaultConfig = {
 		'live-cmd',
 		{
+			command_name = 'Preview',
+			enable_highlighting = true,
+			inline_highlighting = true,
+			hl_groups = {
+				insertion = 'DiffAdd',
+				deletion = 'DiffDelete',
+				change = 'DiffChange',
+			},
+
 			commands = {
 				Norm = { cmd = 'norm' },
-				Reg = {
-					cmd = 'norm',
-					-- This will transform ":5Reg a" into ":norm 5@a"
-					args = function(opts)
-						return (opts.count == -1 and '' or opts.count) .. '@' .. opts.args
-					end,
-					range = '',
-				},
-
 			},
-
-			defaults = {
-				enable_highlighting = true,
-				inline_highlighting = true,
-				hl_groups = { insertion = 'DiffAdd', deletion = 'DiffDelete', change = 'DiffChange' },
-			},
-
-			debug = false,
 		},
 	},
 }
