283f22accfbbe66d28d7898c72321e0048c5b366
ADoyle <adoyle.h@gmail.com>
2025-06-12 18:30:11 +0800

    feat: use chrisgrieser/nvim-spider instead of bkad/CamelCaseMotion

--- lua/one/plugins/move/init.lua
+++ lua/one/plugins/move/init.lua
@@ -3,7 +3,7 @@ local M = { 'move', desc = 'Plugins for fast cursor moving' }
 M.deps = {
 	require('one.plugins.move.flash'),
 	require('one.plugins.move.window-picker'),
-	require('one.plugins.move.caml-case'),
+	require('one.plugins.move.nvim-spider'),
 	require('one.plugins.move.accelerated'),
 	require('one.plugins.move.line'),
 	require('one.plugins.move.climber'),
