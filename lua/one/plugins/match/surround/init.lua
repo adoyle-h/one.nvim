a72be15ff80b608ca5fa28a13f72d44135705fd5
ADoyle <adoyle.h@gmail.com>
2026-04-24 03:57:26 +0800

    fix(nvim-surround): migrate v3 to v4
    Signed-off-by: ADoyle <adoyle.h@gmail.com>

--- lua/one/plugins/match/surround/init.lua
+++ lua/one/plugins/match/surround/init.lua
@@ -3,16 +3,20 @@ return {
 		{
 			'kylechui/nvim-surround',
 			desc = 'mappings to easily delete, change and add such surroundings in pairs, such as quotes, parens, tags etc.',
+
 			config = function(config)
+				vim.g.nvim_surround_no_visual_mappings = true
+
 				require('nvim-surround').setup(config.surround)
+
+				vim.keymap.set('x', 's', '<Plug>(nvim-surround-visual)', {
+					desc = 'Add a surrounding pair around a visual selection',
+				})
+				vim.keymap.set('x', 'gs', '<Plug>(nvim-surround-visual-line)', {
+					desc = 'Add a surrounding pair around a visual selection, on new lines',
+				})
 			end,
 
-			defaultConfig = { 'surround', {
-				keymaps = {
-					visual = 's',
-					visual_line = 'gs',
-				},
-			} },
 		},
 
 		require('one.plugins.match.surround.markdown'),
